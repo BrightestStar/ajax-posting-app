4deca6bcb8ba48b447257a383233aa1d00033649
lucas <d2194499406@gmail.com>
2017-07-19 13:19:36 +0800

    could like and cancel like

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :posts
-  
+  resources :posts do
+    member do
+      post "like" => "posts#like"
+      post "unlike" => "posts#unlike"
+    end
+  end
+
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery-2" => "pages#jquery_2"
   get "/jquery-3" => "pages#jquery_3"
